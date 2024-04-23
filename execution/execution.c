a64b1918a8c4b26a2199b50229ce55654d5dd616
Hamza Boudar <hboudar@e3r8p9.1337.ma>
2024-04-23 08:56:29 +0100

    added all

--- execution/execution.c
+++ execution/execution.c
@@ -6,10 +6,25 @@
 /*   By: hboudar <hboudar@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/20 11:36:55 by hboudar           #+#    #+#             */
-/*   Updated: 2024/04/20 16:57:52 by hboudar          ###   ########.fr       */
+/*   Updated: 2024/04/23 08:54:58 by hboudar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "execution.h"
 
-line = readline("\033[1;34m➜ minishell \033[0m");
+int execution(t_prompt *prompt, char *env[])
+{
+    if (1)
+        execute_one_cmd(prompt, env);
+    else if ("pipe")
+        execute_pipe(prompt, env);
+}
+
+int main(int ac, char **av, char *env[])
+{
+    t_prompt prompt;
+
+    
+    execution(&prompt, env);
+    return (0);
+}
