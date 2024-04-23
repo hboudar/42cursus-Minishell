c491c12011324258dedd1f84e8b5277bbac78017
Hamza Boudar <hboudar@e3r8p9.1337.ma>
2024-04-23 15:12:50 +0100

    added all

--- execution/execution.c
+++ execution/execution.c
@@ -3,30 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   execution.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aoulahra <aoulahra@student.42.fr>          +#+  +:+       +#+        */
+/*   By: hboudar <hboudar@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/20 11:36:55 by hboudar           #+#    #+#             */
-<<<<<<< HEAD
-<<<<<<< HEAD
-/*   Updated: 2024/04/20 16:58:25 by aoulahra         ###   ########.fr       */
-=======
-/*   Updated: 2024/04/23 08:54:58 by hboudar          ###   ########.fr       */
->>>>>>> a64b1918a8c4b26a2199b50229ce55654d5dd616
-=======
-/*   Updated: 2024/04/23 13:08:57 by hboudar          ###   ########.fr       */
->>>>>>> c16e03627442c4373386e6ddc305ea6878482c24
+/*   Updated: 2024/04/23 15:12:22 by hboudar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "execution.h"
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-int execution(t_prompt *prompt, char *env[])
-=======
 int one_prompt(t_prompt *prompt, char *env[])
->>>>>>> c16e03627442c4373386e6ddc305ea6878482c24
 {
     if (prompt->cmd->type == CMD)
     {
@@ -48,4 +34,3 @@ int execution(t_prompt *prompt, char *env[])
             execution(prompt->right, env);
     }
 }
->>>>>>> a64b1918a8c4b26a2199b50229ce55654d5dd616
