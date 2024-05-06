5b05f5b2e456fedd9d781ef62dbccb4846603016
Hamza Boudar <hboudar@e3r7p10.1337.ma>
2024-05-06 21:36:21 +0100

    mdf

--- execution/builtins/ft_echo.c
+++ execution/builtins/ft_echo.c
@@ -6,31 +6,33 @@
 /*   By: hboudar <hboudar@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/25 09:01:10 by hboudar           #+#    #+#             */
-/*   Updated: 2024/05/06 16:07:59 by hboudar          ###   ########.fr       */
+/*   Updated: 2024/05/06 21:35:39 by hboudar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../execution.h"
 
-static void echo_without_newline(t_prompt *prompt)
+static int check_options(char *str)
 {
     int i;
 
+    if (str[0] != '-')
+        return (0);
     i = 1;
-    while (prompt->cmd->args[i])
+    while (str[i])
     {
-        ft_putstr_fd(prompt->cmd->args[i], 1);
-        if (prompt->cmd->args[i + 1])
-            ft_putstr_fd(" ", 1);
+        if (str[i] != 'n')
+            return (0);
         i++;
     }
+    return (1);
 }
 
-static void echo_with_newline(t_prompt *prompt)
+static void ft_print(t_prompt *prompt, int option)
 {
     int i;
 
-    i = 1;
+    i = option + 1;
     while (prompt->cmd->args[i])
     {
         ft_putstr_fd(prompt->cmd->args[i], 1);
@@ -38,18 +40,30 @@ static void echo_with_newline(t_prompt *prompt)
             ft_putstr_fd(" ", 1);
         i++;
     }
-    ft_putstr_fd("\n", 1);
+    if (option == 0)
+        ft_putstr_fd("\n", 1);
 }
 
 int ft_echo(t_prompt *prompt, t_env *env)
 {
+    int i;
+    int option;
+
     (void)env;
     if (prompt->cmd->args[1])
     {
-        if (!ft_strncmp(prompt->cmd->args[1], "-n", 3))
-            echo_without_newline(prompt);
-        else
-            echo_with_newline(prompt);
-    } 
+        (1) && (option = 0, i = 1);
+        while (prompt->cmd->args[i] != NULL)
+        {
+            if (check_options(prompt->cmd->args[i]))
+            {
+                option++;
+                i++;
+            }
+            else
+                break ;
+        }
+        ft_print(prompt, option);
+    }
     return (0);
 }
