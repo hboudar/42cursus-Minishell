4fe9cc12b297afdf4c8c8820118ade36a72f5188
Hamza Boudar <hboudar@e3r7p8.1337.ma>
2024-05-23 21:08:26 +0100

    mdf

--- execution/cmd/ft_cd.c
+++ execution/cmd/ft_cd.c
@@ -6,7 +6,7 @@
 /*   By: hboudar <hboudar@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/25 09:01:04 by hboudar           #+#    #+#             */
-/*   Updated: 2024/05/20 15:49:14 by hboudar          ###   ########.fr       */
+/*   Updated: 2024/05/22 23:32:48 by hboudar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,23 @@ void set_oldpwd(char *oldpwd, t_env *env)
 {
     while (env)
     {
-        if (!ft_strncmp(env->key, "OLDPWD=", 8))
+        if (!ft_strncmp(env->key, "OLDPWD", 7))
         {
             if (!env->value)
+            {
                 env->value = oldpwd;
+                free(env->key);
+                env->key = ft_strdup("OLDPWD=");
+                if (!env->key)
+                    return ;
+                env->print = PRINT;
+            }
             else
             {
                 free(env->value);
                 env->value = NULL;
                 env->value = oldpwd;
+                env->print = PRINT;
             }
         }
         env = env->next;
