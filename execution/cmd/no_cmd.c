7126f7036565c8aea8b34d8ebb8ade646d161b5f
Hamza Boudar <hboudar@e3r7p8.1337.ma>
2024-05-24 16:14:06 +0100

    mdf

--- execution/cmd/no_cmd.c
+++ execution/cmd/no_cmd.c
@@ -6,7 +6,7 @@
 /*   By: hboudar <hboudar@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/10 13:13:54 by hboudar           #+#    #+#             */
-/*   Updated: 2024/05/23 21:39:20 by hboudar          ###   ########.fr       */
+/*   Updated: 2024/05/24 16:02:38 by hboudar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,79 +43,144 @@ static int	ft_inredirect(t_prompt *prompt, int *fd, int *fd0)
     return (1);
 }
 
-// Setup custom signal handlers
-void setup_signal_handlers(void (*int_handler)(int), void (*quit_handler)(int)) {
-    // rl_catch_signals = 0;
-    signal(SIGINT, int_handler);
-    signal(SIGQUIT, quit_handler);
-}
 
-// Restore default signal handlers
+// static void	here_doc2(t_prompt *prompt, int *fd, int i)
+// {
+//     extern int g_caught;
+// 	char	*str;
+//     char    *limiter;
+//     setup_signal_handlers(handle_sigint, SIG_IGN);
+//     close(fd[0]);
+//     limiter = prompt->cmd->limiter[i];
+// 	while (1)
+// 	{
+// 		str = readline("> ");
+// 		if (str != NULL && g_caught)
+// 		{
+//             printf("dkhelll\n");
+//             g_caught = 0;
+// 			free(str);
+// 			str = NULL;
+//             break ;
+// 		}
+// 		free(str);
+// 		str = NULL;
+// 	}
+//     restore_default_signal_handlers();
+//     exit(0);
+// }
+
+// static void here_doc(t_prompt *prompt, int i)
+// {
+//     pid_t pid;
+//     int fd[2];
+//     if (pipe(fd) == -1)
+//     {
+//         printf("pipe error\n");
+//         return ;
+//     }
+//     struct sigaction sa_ignore, sa_orig_int;
+//     sa_ignore.sa_handler = SIG_IGN;
+//     sigaction(SIGINT, &sa_ignore, &sa_orig_int);
+//     pid = fork();
+//     if (pid == -1)
+//     {
+//         printf("fork error\n");
+//         close(fd[0]);
+//         close(fd[1]);
+//         return ;
+//     }
+//     if (pid == 0)
+//         here_doc2(prompt, fd, i);
+//     else
+//     {
+//         waitpid(pid, &prompt->exit_state, 0);
+//         prompt->exit_state = WEXITSTATUS(prompt->exit_state);
+//         (1) && (close(fd[1]), dup2(fd[0], 0));
+//     }
+// }
+
 void restore_default_signal_handlers() {
     signal(SIGINT, SIG_DFL);
     signal(SIGQUIT, SIG_DFL);
 }
 
-static void	here_doc2(t_prompt *prompt, int *fd, int i)
+void sigint_handler_heredoc(int sig)
 {
     extern int g_caught;
-	char	*str;
-    char    *limiter;
-
-    close(fd[0]);
-    setup_signal_handlers(handler, SIG_IGN);
-    limiter = prompt->cmd->limiter[i];
-	while (1)
-	{
-		str = readline("> ");
-        rl_clear_history();
-		rl_on_new_line();
-		rl_replace_line("", 0);
-		rl_redisplay();
-        add_history(str);
-		if ((ft_strlen(str) == ft_strlen(limiter)
-			&& !ft_strncmp(str, limiter, ft_strlen(limiter))) || !str || g_caught)
-		{
-            g_caught = 0;
-			free(str);
-			str = NULL;
-            break ;
-		}
-		free(str);
-		str = NULL;
-	}
-    restore_default_signal_handlers();
-    exit(0);
+    if (sig == SIGINT) {
+        printf("\nReceived SIGINT (Control-C)\n");
+        restore_default_signal_handlers();
+        g_caught = 1;
+        // rl_replace_line("", 0);
+        rl_on_new_line();
+        rl_redisplay();
+        exit(1);
+    }
 }
 
-static void here_doc(t_prompt *prompt, int i)
-{
-    pid_t pid;
-    int fd[2];
-
-    if (pipe(fd) == -1)
-    {
-        printf("pipe error\n");
-        return ;
+// Custom signal handler for SIGINT
+void sigint_handler(int sig) {
+    extern int g_caught;
+    if (sig == SIGINT) {
+        printf("\nReceived SIGINT (Control-C)\n");
+        g_caught = 1;
+        // rl_replace_line("", 0); 
+        rl_on_new_line(); 
+        rl_redisplay();
     }
+}
+
+void setup_signal_handlers(void (*int_handler)(int), void (*quit_handler)(int)) {
+    signal(SIGINT, int_handler);
+    signal(SIGQUIT, quit_handler);
+}
+
+
+void heredoc_mode() {
+    extern int g_caught;
     struct sigaction sa_ignore, sa_orig_int;
     sa_ignore.sa_handler = SIG_IGN;
     sigaction(SIGINT, &sa_ignore, &sa_orig_int);
-    pid = fork();
-    if (pid == -1)
-    {
-        printf("fork error\n");
-        close(fd[0]);
-        close(fd[1]);
-        return ;
-    }
-    if (pid == 0)
-        here_doc2(prompt, fd, i);
-    else
-    {
-        waitpid(pid, &prompt->exit_state, 0);
-        prompt->exit_state = WEXITSTATUS(prompt->exit_state);
-        (1) && (g_caught = 0, close(fd[1]), dup2(fd[0], 0));
+
+    pid_t pid = fork();
+
+    if (pid == 0) {
+
+        char* heredoc_input;
+
+        setup_signal_handlers(sigint_handler_heredoc, SIG_IGN);
+
+        printf("Entering heredoc mode. Type 'EOF' to end.\n");
+
+        while ((heredoc_input = readline("heredoc> ")) != NULL) {
+            if (g_caught) {
+                printf("Here\n");
+                free(heredoc_input);
+                g_caught = 0;
+                break;
+            }
+
+            if (strcmp(heredoc_input, "EOF") == 0) {
+                free(heredoc_input);
+                break;
+            }
+
+            printf("Heredoc input: %s\n", heredoc_input);
+            free(heredoc_input);
+        }
+        restore_default_signal_handlers();
+        exit(0);
+    } else if (pid > 0) {
+        int status;
+        waitpid(pid, &status, 0);
+        setup_signal_handlers(sigint_handler, SIG_IGN);
+
+
+    } else {
+        // Fork failed
+        perror("fork");
+        exit(EXIT_FAILURE);
     }
 }
 
@@ -130,7 +195,8 @@ int    no_cmd(t_prompt *prompt)
     if (prompt->cmd->type == HERE_DOC)
     {
         while (prompt->cmd->limiter[++i])
-            here_doc(prompt, i);
+            heredoc_mode();
+            // here_doc(prompt, i);
     }
     if (pipe(fd) == -1)
         return (perror("pipe failed"), 1);
