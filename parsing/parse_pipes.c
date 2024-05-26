38ce34ea5d2fa41295e6450937519dd784dd4d5a
ayouboul21 <107890565+ayouboul21@users.noreply.github.com>
2024-05-26 20:02:01 +0100

    parsed pipe

--- parsing/parse_pipes.c
+++ parsing/parse_pipes.c
@@ -6,7 +6,7 @@
 /*   By: aoulahra <aoulahra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/26 18:54:04 by aoulahra          #+#    #+#             */
-/*   Updated: 2024/05/26 19:17:45 by aoulahra         ###   ########.fr       */
+/*   Updated: 2024/05/26 20:01:03 by aoulahra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,50 +14,90 @@
 
 int	check_pipe(t_token *token)
 {
-    t_token	*tmp;
+	t_token	*tmp;
 
-    tmp = token;
-    while (tmp)
-    {
-        if (tmp->type == PIPE_TKN)
-            return (1);
-        tmp = tmp->next;
-    }
-    return (0);
+	tmp = token;
+	while (tmp)
+	{
+		if (tmp->type == PIPE_TKN)
+			return (1);
+		tmp = tmp->next;
+	}
+	return (0);
 }
 
 t_token	*get_pipe(t_token *token)
 {
-    t_token	*tmp;
+	t_token	*tmp;
 
-    tmp = token;
-    while (tmp)
-    {
-        if (tmp->type == PIPE_TKN)
-            return (tmp->next);
-        tmp = tmp->next;
-    }
-    return (NULL);
+	tmp = token;
+	while (tmp)
+	{
+		if (tmp->type == PIPE_TKN)
+			return (tmp->next);
+		tmp = tmp->next;
+	}
+	return (NULL);
 }
 
-void	handle_pipe(t_prompt **prmpt, t_token **token, t_env *env)
+void	add_token(t_token **token, t_token *pipeless)
+{
+	t_token	*tmp;
+
+	tmp = *token;
+	if (!tmp)
+	{
+		*token = pipeless;
+		return ;
+	}
+	while (tmp->next)
+		tmp = tmp->next;
+	tmp->next = pipeless;
+	tmp->next->next = NULL;
+}
+
+t_token	*remove_pipe(t_token *token)
+{
+	t_token	*tmp;
+	t_token	*tmp2;
+	t_token	*pipeless;
+
+	pipeless = malloc(sizeof(t_token));
+	tmp = token;
+	while (tmp)
+	{
+		if (tmp->type == PIPE_TKN)
+		{
+			tmp2 = tmp->next;
+			remove_token(&token, tmp);
+            pipeless->next = NULL;
+			return (pipeless);
+		}
+		add_token(&pipeless, tmp);
+		tmp = tmp->next;
+	}
+	return (pipeless);
+}
+
+void	parse_pipes(t_prompt **prmpt, t_token **token, t_env *env)
 {
 	t_token	*tmp;
 	t_token	*pipeless;
 
+    // print_tokens(*token);
 	if (!check_pipe(*token))
 	{
 		(*prmpt)->left = NULL;
 		(*prmpt)->right = NULL;
 		(*prmpt)->type = P_CMD;
-		parse_cmd(*token, env);
+		(*prmpt)->cmd = parse_cmd(*token, env);
 		return ;
 	}
 	tmp = get_pipe(*token);
 	pipeless = remove_pipe(*token);
 	(*prmpt)->left = malloc(sizeof(t_prompt));
 	(*prmpt)->right = malloc(sizeof(t_prompt));
-    handle_pipe(&(*prmpt)->left, &pipeless, env);
-    handle_pipe(&(*prmpt)->right, &tmp, env);
+	parse_pipes(&(*prmpt)->left, &pipeless, env);
+	parse_pipes(&(*prmpt)->right, &tmp, env);
 	(*prmpt)->type = P_PIPE;
 }
