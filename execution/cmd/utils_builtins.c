4cecc5e4bc30417ea7cb2ce948b823945654e41e
Hamza Boudar <hboudar@e3r7p8.1337.ma>
2024-05-27 10:19:36 +0100

    rederictions/here_doc in one_cmd are done

--- execution/cmd/utils_builtins.c
+++ execution/cmd/utils_builtins.c
@@ -6,7 +6,7 @@
 /*   By: hboudar <hboudar@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/25 08:46:04 by hboudar           #+#    #+#             */
-/*   Updated: 2024/05/26 19:56:56 by hboudar          ###   ########.fr       */
+/*   Updated: 2024/05/27 10:17:38 by hboudar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,13 @@
 
 static int  ft_outredirect(t_prompt *prompt, int *fd, int *fd1)
 {
-    printf("ft_outredirect\n");
     if (prompt->cmd->file->type == 1)
-        *fd1 = open(prompt->cmd->file->data, O_CREAT | O_WRONLY | O_TRUNC, 0644);
+        *fd1 = open(prompt->cmd->file->data, O_CREAT | O_RDWR | O_TRUNC, 0644);
     else if (prompt->cmd->file->type == 2)
         *fd1 = open(prompt->cmd->file->data, O_CREAT | O_WRONLY | O_APPEND, 0644);
     if (*fd1 == -1)
     {
-        printf("Error: %s: %s\n", prompt->cmd->file->next->data, strerror(errno));
+        printf("Error: %s: %s\n", prompt->cmd->file->data, strerror(errno));
         close(fd[0]);
         close(fd[1]);
         return (0);
@@ -35,21 +34,16 @@ static int  ft_outredirect(t_prompt *prompt, int *fd, int *fd1)
         perror("dup2");
         return (0);
     }
-    close(*fd1);
-    *fd1 = 1;
     return (1);
 }
 
 static int	ft_inredirect(t_prompt *prompt, int *fd, int *fd0)
 {
-    printf("ft_inredirect\n");
     if (!prompt->cmd->file->type)
     {
         (*fd0 != 0) && (close(*fd0));
         *fd0 = open(prompt->cmd->file->data, O_RDONLY);
     }
-    else if (dup2(fd[0], *fd0) == -1)
-            return (printf("failed dup2"),0);
     if (*fd0 == -1)
     {
         close(fd[0]);
@@ -57,29 +51,33 @@ static int	ft_inredirect(t_prompt *prompt, int *fd, int *fd0)
         perror(prompt->cmd->file->data);
         return (0);
     }
-    if (dup2(*fd0, 0) == -1)
-    {
-        close(fd[0]);
-        close(fd[1]);
-        close(*fd0);
-        perror("dup2");
-        return (0);
-    }
     return (1);
 }
-static int ft_redirection(t_prompt *prompt, int *fd, int *fd0, int *fd1)
+
+static int ft_redirection(t_prompt *prompt, t_env **env, int *fd)
 {
-    *fd0 = 0;
-    *fd1 = 1;
+    int	real_in;
+	int	real_out;
+    int	fd0;
+	int	fd1;
+
+    (1) && (real_in = dup(0), printf("real_in = %d\n", real_in));
+    (1) && (real_out = dup(1), printf("real_out = %d\n", real_out));
+    (1) && (fd0 = 0, fd1 = 1);
     while (prompt->cmd->file != NULL)
     {
         if ((!prompt->cmd->file->type || prompt->cmd->file->type == 3)
-            && !ft_inredirect(prompt, fd, fd0))
+            && !ft_inredirect(prompt, fd, &fd0))
             return (1);
-        else if (prompt->cmd->file->type && !ft_outredirect(prompt, fd, fd1))
+        else if (prompt->cmd->file->type && !ft_outredirect(prompt, fd, &fd1))
             return (1);
         prompt->cmd->file = prompt->cmd->file->next;
     }
+    prompt->exit_state = ft_builtin(prompt, env);
+    (fd0 != 0) && (close(fd0));
+    (fd1 != 1) && (close(fd1));
+    (1) && (dup2(real_in, 0), close(real_in));
+    (1) && (dup2(real_out, 1), close(real_out));
     return (0);
 }
 
@@ -87,25 +85,18 @@ int	execute_builtin(t_prompt *prompt, t_env **env)
 {
     extern int g_caught;
     int	fd[2];
-	int	fd0;
-	int	fd1;
     int i;
-
+    
+    
 	(1) && (i = -1, g_caught = 0);
-    // while (prompt->cmd->type == HERE_DOC && !g_caught && prompt->cmd->limiter[++i])
-    //     here_doc(prompt, i, fd);
+    while (prompt->cmd->type == HERE_DOC && !g_caught && prompt->cmd->limiter[++i])
+        here_doc(prompt, i, fd);
     if (g_caught)
     {
-        printf("g_caught\n");
         close(fd[0]);
         return (1);
     }
-    printf("b:here\n");
-    if (ft_redirection(prompt, fd, &fd0, &fd1))
+    if (ft_redirection(prompt, env, fd))
         return (1);
-    printf("a:here\n");
-	prompt->exit_state = ft_builtin(prompt, env);
-    (fd0 != 0) && (close(fd0));
-    (fd1 != 1) && (close(fd1));
 	return (prompt->exit_state);
 }
