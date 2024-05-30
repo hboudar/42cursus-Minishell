deb420c4a1cd53c2f7bcbb5d5b1b75a67a6e3089
Hamza Boudar <hboudar@e3r7p8.1337.ma>
2024-05-30 20:36:43 +0100

    mdf

--- execution/cmds/ft_or.c
+++ execution/cmds/ft_or.c
@@ -1,20 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_and.c                                           :+:      :+:    :+:   */
+/*   ft_or.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hboudar <hboudar@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/05/29 20:16:38 by hboudar           #+#    #+#             */
-/*   Updated: 2024/05/29 20:17:24 by hboudar          ###   ########.fr       */
+/*   Created: 2024/05/29 20:17:08 by hboudar           #+#    #+#             */
+/*   Updated: 2024/05/30 20:36:23 by hboudar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../../execution.h"
+# include "../execution.h"
 
-int ft_and(t_prompt *prompt, t_env **env)
+int	ft_or(t_prompt *prompt, t_env **env)
 {
-    if (!execution(prompt->left, env))
-        return (execution(prompt->right, env));
-    return (prompt->exit_state);
+	if (execution(prompt->left, env))
+		return (execution(prompt->right, env));
+	return (prompt->exit_state);
 }
