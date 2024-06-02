738e46680d845e1fab46d30888e5d111408cbc0d
Hamza Boudar <hboudar@e3r8p9.1337.ma>
2024-06-02 14:50:07 +0100

    mdf

--- execution/cmds/ft_or.c
+++ execution/cmds/ft_or.c
@@ -6,7 +6,7 @@
 /*   By: hboudar <hboudar@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/29 20:17:08 by hboudar           #+#    #+#             */
-/*   Updated: 2024/06/02 13:19:59 by hboudar          ###   ########.fr       */
+/*   Updated: 2024/06/02 14:49:51 by hboudar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 int	ft_or(t_prompt *prompt, t_env **env, int *fd_out, int *fd_in)
 {
-	if (execution(prompt->left, env, fd_out, fd_in))
-		return (execution(prompt->right, env, fd_out, fd_in));
+	if (execution(prompt->left, env, *fd_out, *fd_in))
+		return (execution(prompt->right, env, *fd_out, *fd_in));
 	return (prompt->exit_state);
 }
