2eb8e84540762e628a0e995a16dca80a20e259c9
Hamza Boudar <hboudar@e3r7p4.1337.ma>
2024-06-05 20:28:39 +0100

    mdf

--- execution/cmds/ft_pipe.c
+++ execution/cmds/ft_pipe.c
@@ -6,7 +6,7 @@
 /*   By: hboudar <hboudar@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/08 14:16:57 by hboudar           #+#    #+#             */
-/*   Updated: 2024/06/04 23:41:42 by hboudar          ###   ########.fr       */
+/*   Updated: 2024/06/05 20:22:29 by hboudar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,6 +33,7 @@ void	do_left(t_prompt *prompt, t_env **env)
 	int		fd[2];
 	pid_t	pid;
 
+	expand_cmd(prompt, *env);
 	ignore_signals();
 	if (pipe(fd) == -1)
 		error("pipe");
@@ -56,6 +57,7 @@ int	do_right(t_prompt *prompt, t_env **env)
 {
 	pid_t	pid;
 
+	expand_cmd(prompt, *env);
 	ignore_signals();
 	pid = fork();
 	if (pid == -1)
