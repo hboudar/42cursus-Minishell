09a53358735d2d9dbd879b727aefae8c6b011bb6
Hamza Boudar <hboudar@e3r7p5.1337.ma>
2024-06-09 19:30:47 +0100

    mdf

--- execution/execution.c
+++ execution/execution.c
@@ -6,16 +6,74 @@
 /*   By: hboudar <hboudar@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/20 11:36:55 by hboudar           #+#    #+#             */
-/*   Updated: 2024/06/09 16:30:33 by hboudar          ###   ########.fr       */
+/*   Updated: 2024/06/09 19:26:43 by hboudar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "execution.h"
 
+static void	here_doc2(t_prompt *prompt, int fd, char *limiter)
+{
+    extern int g_caught;
+	char	*str;
+
+    setup_signal_handlers(sigint_handler_heredoc, SIG_IGN);
+    while (1)
+    {
+        str = readline("> ");
+        //expanding the variables
+        if (g_caught || (!ft_strncmp(str, limiter, ft_strlen(limiter))
+            && ft_strlen(str) == ft_strlen(limiter)) || !str)
+        {
+            if (str)
+                free(str);
+            break;
+        }
+        write(fd, str, ft_strlen(str));
+        write(fd, "\n", 1);
+        free(str);
+    }
+    exit(0);
+}
+
+void	here_doc_to_input(t_file *file, t_limiter *limiter, int fd_doc)
+{
+	pid_t	pid;
+
+	unlink("/tmp/.doc");
+	fd_doc = open("/tmp/.doc", O_RDWR | O_CREAT | O_TRUNC, 0644);
+	while (file && file->type != 3)
+		file = file->next;
+	if (!file)
+		return ;
+	ignore_signals();
+	pid = fork();
+	if (pid == 0)
+		here_doc2(file, fd_doc, *limiter);
+	else
+	{
+		waitpid(pid, NULL, 0);
+		close(fd_doc);
+	}
+}
+
+void	do_here_doc(t_prompt *prompt)
+{
+	if (prompt->subshell)
+		here_doc_to_input(prompt->file, prompt->limiter, 0);
+	if (prompt->type == P_CMD)
+		here_doc_to_input(prompt->cmd->file, prompt->cmd->limiter, 0);
+	else
+	{
+		do_here_doc(prompt->left);
+		do_here_doc(prompt->right);
+	}
+}
+
 int	execution(t_prompt *prompt, t_env **env, int std_in)
 {
 	printf("execution\n");
-	if (prompt->subshell && prompt->type != P_CMD)
+	if (prompt->subshell)
 		return (subshell(prompt, env));
 	else if (prompt->type == P_CMD)
 		prompt->exit_state = ft_cmd(prompt, env);
