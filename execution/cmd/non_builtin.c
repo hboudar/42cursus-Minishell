cc9a2f7dd84f216d127d5014c980e64b1624424d
Cloneg7 <boudarhamza12@gmail.com>
2024-06-14 12:14:16 +0100

    mdf

--- execution/cmd/non_builtin.c
+++ execution/cmd/non_builtin.c
@@ -6,7 +6,7 @@
 /*   By: hboudar <hboudar@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/08 08:43:21 by hboudar           #+#    #+#             */
-/*   Updated: 2024/06/14 12:11:37 by hboudar          ###   ########.fr       */
+/*   Updated: 2024/06/14 12:13:36 by hboudar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,9 +35,9 @@ static void	child_process(t_prompt *prompt, t_env *env)
 		printf("minishell: %s: permission denied\n", prompt->cmd->args[0]);
 		exit(126);
 	}
-	(1) && (envp = env_to_envp(env, env), execve(path, prompt->cmd->args, envp));
-	if (prompt->cmd->args)
-		perror(prompt->cmd->args[0]);
+	(1) && (envp = env_to_envp(env, env),
+		execve(path, prompt->cmd->args, envp));
+	(prompt->cmd->args) && (perror(prompt->cmd->args[0]), free_envp(envp));
 	exit(127);
 }
 
