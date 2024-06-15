b4d03fbbbf4c701f037cf80304d7bdb88ee54d4a
ayouboul21 <oulahraiouu@gmail.com>
2024-06-15 10:56:44 +0100

    added notes

---
+++ notes.r
@@ -0,0 +1,7 @@
+ctrl c exit state
+export key{anything except alpha or = or _}+=value shouldnt work
+limiter state should change after joining them
+reset the in and out fds between the and/or commands
+^\ should return 131 in child process
+fork every pipe then execute the pipes to get the correct exit state
+expand * in execution after the normal expand
