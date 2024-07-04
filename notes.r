7cd1c7439cabdd88461ae3bc782380e28ca43844
ayouboul21 <oulahraiouu@gmail.com>
2024-07-04 11:40:59 +0100

    added idea for expand *

--- notes.r
+++ notes.r
@@ -10,4 +10,34 @@ reset the in and out fds between the and/or commands : fixed
 
 fork every pipe then execute the pipes to get the correct exit state : to be disscussed
 
-expand * in execution after the normal expand
+expand * in execution after the normal expand : make your own match function and you should be good to go
+
+
+// Function to check if a string matches a wildcard pattern
+int wildcard_match(const char *pattern, const char *str)
+{
+    while (*pattern)
+    {
+        if (*pattern == '*')
+        {
+            pattern++;
+            if (!*pattern)
+                return 1;
+            while (*str)
+            {
+                if (wildcard_match(pattern, str))
+                    return 1;
+                str++;
+            }
+            return 0;
+        }
+        else if (*pattern == *str)
+        {
+            pattern++;
+            str++;
+        }
+        else
+            return 0;
+    }
+    return !*str;
+}
